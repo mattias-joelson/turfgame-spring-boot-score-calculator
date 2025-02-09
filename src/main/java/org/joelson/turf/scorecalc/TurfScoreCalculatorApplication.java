ac3e05522acfd8014d965ecedacbf32d58c192b9
Mattias Joëlson <mattias@joelson.org>
2025-02-09 09:13:11 +0100

    Skeleton FeedImporterService.

--- src/main/java/org/joelson/turf/scorecalc/TurfScoreCalculatorApplication.java
+++ src/main/java/org/joelson/turf/scorecalc/TurfScoreCalculatorApplication.java
@@ -1,13 +1,46 @@
 package org.joelson.turf.scorecalc;
 
+import org.joelson.turf.scorecalc.service.FeedImporterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TurfScoreCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TurfScoreCalculatorApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(TurfScoreCalculatorApplication.class);
 
+    @Autowired
+    FeedImporterService feedImporterService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurfScoreCalculatorApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner argumentHandler(ApplicationContext ctx) {
+        return args -> {
+            // arguments passed through -Dspring-boot.run.arguments="test1 test2"
+            logArray("Program arguments:", "No program arguments.", args);
+            feedImporterService.importFeeds(args);
+        };
+    }
+
+    private void logArray(String hasElements, String noElements, String[] strings) {
+        if (strings.length > 0) {
+            logger.info(hasElements);
+            int maxLength = String.valueOf(strings.length).length();
+            String format = String.format("  [%%%dd] %%s", maxLength);
+            for (int i = 0; i < strings.length; i += 1) {
+                logger.info(String.format(format, i, strings[i]));
+            }
+        } else {
+            logger.info(noElements);
+        }
+    }
 }
--- src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
+++ src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
@@ -1,4 +1,41 @@
 package org.joelson.turf.scorecalc.service;
 
+import org.joelson.turf.turfgame.apiv5.FeedTakeover;
+import org.joelson.turf.turfgame.util.FeedsReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class FeedImporterService {
+
+    private static final Logger logger = LoggerFactory.getLogger(FeedImporterService.class);
+
+    private final FeedsReader feedsReader;
+
+    private int filesHandled = 0;
+    private int filesSkipped = 0;
+    private final List<String> filesWithErrors;
+
+    public FeedImporterService() {
+        feedsReader = new FeedsReader(Map.of("takeover", FeedTakeover.class));
+        filesWithErrors = new ArrayList<>();
+    }
+
+    public void importFeeds(String[] args) {
+        for (String filename : args) {
+            importFeed(filename);
+        }
+        if (args.length > 0) {
+            logger.info("Done importing data.");
+        }
+    }
+
+    public void importFeed(String filename) {
+        logger.info(String.format("Importing data from '%s'", filename));
+    }
 }
