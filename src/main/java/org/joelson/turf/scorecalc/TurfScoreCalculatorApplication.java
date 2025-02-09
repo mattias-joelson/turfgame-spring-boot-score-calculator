d7e870d1beaeedbbfcb2954d75e7dad16b6d4353
Mattias Joëlson <mattias@joelson.org>
2025-02-09 08:24:03 +0100

    Boiler plate code from start.spring.io and Postgres DB setup.

---
+++ src/main/java/org/joelson/turf/scorecalc/TurfScoreCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.joelson.turf.scorecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfScoreCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfScoreCalculatorApplication.class, args);
+	}
+
+}
