f9f85d70fd8e99ae9606a15c513b7ca626b12b5d
Mattias Joëlson <mattias@joelson.org>
2025-02-10 22:49:04 +0100

    Depend om turfgame-resources 1.5.0, FeedImporterService scans through files.

--- src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
+++ src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
@@ -1,13 +1,16 @@
 package org.joelson.turf.scorecalc.service;
 
+import org.joelson.turf.turfgame.FeedObject;
 import org.joelson.turf.turfgame.apiv5.FeedTakeover;
+import org.joelson.turf.turfgame.util.DefaultFeedContentErrorHandler;
+import org.joelson.turf.turfgame.util.DefaultFeedContentLoggerErrorHandler;
 import org.joelson.turf.turfgame.util.FeedsReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Map;
 
 @Service
@@ -16,14 +19,14 @@ public class FeedImporterService {
     private static final Logger logger = LoggerFactory.getLogger(FeedImporterService.class);
 
     private final FeedsReader feedsReader;
+    private final DefaultFeedContentErrorHandler errorHandler;
 
     private int filesHandled = 0;
-    private int filesSkipped = 0;
-    private final List<String> filesWithErrors;
+    private int takeovers = 0;
 
     public FeedImporterService() {
-        feedsReader = new FeedsReader(Map.of("takeover", FeedTakeover.class));
-        filesWithErrors = new ArrayList<>();
+        errorHandler = new DefaultFeedContentLoggerErrorHandler(logger);
+        feedsReader = new FeedsReader(Map.of("takeover", FeedTakeover.class), errorHandler);
     }
 
     public void importFeeds(String[] args) {
@@ -32,10 +35,42 @@ public class FeedImporterService {
         }
         if (args.length > 0) {
             logger.info("Done importing data.");
+            errorHandler.messageErrorPaths(20);
         }
     }
 
     public void importFeed(String filename) {
-        logger.info(String.format("Importing data from '%s'", filename));
+        logger.info("Importing data from '{}'", filename);
+        int filesHandledBefore = filesHandled;
+        int takeoversBefore = takeovers;
+        try {
+            feedsReader.handleFeedObjectPath(Path.of(filename), this::logEvery1000thPath, this::handleFeedObject);
+        } catch (IOException e) {
+            logger.error("Error importing data from '{}'", filename);
+        }
+        int filesHandledAfter = filesHandled;
+        int takeOversAfter = takeovers;
+        logger.info("    files handled: {}, takeovers: {}",
+                filesHandledAfter - filesHandledBefore,
+                takeOversAfter - takeoversBefore);
+    }
+
+    private void logEvery1000thPath(Path path) {
+        if (filesHandled % 1000 == 0) {
+            logger.info("    reading path {}", path);
+        }
+        filesHandled += 1;
+    }
+
+    private void handleFeedObject(FeedObject feedObject) {
+        if (feedObject instanceof FeedTakeover feedTakeover) {
+            handleTakeover(feedTakeover);
+        } else {
+            throw new IllegalArgumentException("Can't handle feed object of type " + feedObject.getClass().getName());
+        }
+    }
+
+    private void handleTakeover(FeedTakeover feedTakeover) {
+        takeovers += 1;
     }
 }
