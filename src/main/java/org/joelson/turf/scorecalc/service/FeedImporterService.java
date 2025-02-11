6b9c9be46db74b4f9361725f5393033ed9419169
Mattias Joëlson <mattias@joelson.org>
2025-02-11 07:10:55 +0100

    Import feed roots in the order they are specified.

--- src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
+++ src/main/java/org/joelson/turf/scorecalc/service/FeedImporterService.java
@@ -4,9 +4,7 @@ import org.joelson.turf.turfgame.FeedObject;
 import org.joelson.turf.turfgame.apiv5.FeedTakeover;
 import org.joelson.turf.turfgame.util.DefaultFeedContentErrorHandler;
 import org.joelson.turf.turfgame.util.DefaultFeedContentLoggerErrorHandler;
-import org.joelson.turf.turfgame.util.FeedsPathComparator;
 import org.joelson.turf.turfgame.util.FeedsReader;
-import org.joelson.turf.util.FilesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -46,7 +44,7 @@ public class FeedImporterService {
         int filesHandledBefore = filesHandled;
         int takeoversBefore = takeovers;
         try {
-            FilesUtil.forEachFile(Path.of(filename), true, new FeedsPathComparator(), this::addFeedsObjects);
+            feedsReader.handleFeedObjectPath(Path.of(filename), this::logEvery1000thPath, this::handleFeedObject);
         } catch (IOException e) {
             logger.error("Error importing data from '{}'", filename);
         }
@@ -55,14 +53,6 @@ public class FeedImporterService {
         logger.info("    files handled={}, takeovers={}",
                 filesHandledAfter - filesHandledBefore,
                 takeoversAfter - takeoversBefore);
-    }
-
-    private void addFeedsObjects(Path path) {
-        try {
-            feedsReader.handleFeedObjectPath(path, this::logEvery1000thPath, this::handleFeedObject);
-        } catch (IOException e) {
-            logger.error("Error importing data from '{}'", path);
-        }
     }
 
     private void logEvery1000thPath(Path path) {
